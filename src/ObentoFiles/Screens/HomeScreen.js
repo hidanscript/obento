838470f0c388d3bf75056478f6055f16fee184de
hidanscript <hidanshira2@gmail.com>
2019-11-11 19:22:51 -0300

    Arreglada toda la grid

--- src/ObentoFiles/Screens/HomeScreen.js
+++ src/ObentoFiles/Screens/HomeScreen.js
@@ -7,20 +7,38 @@ export default class HomeScreen extends Component {
     render() {
         return(
             <div className="homescreen-main">
-                <div className="homescreen-banner-title">
-                    <h1>Obento</h1>
-                    <h2>Online <b>manga</b> library!</h2>
-                </div>
+                <div className="homescreen-cabecera">
+                    <div className="homescreen-banner-title">
+                        <h1>Obento</h1>
+                        <h2>Online <b>manga</b> library!</h2>
+                        <div className="homescreen-searchbar">
+                            <input type="text" placeholder="manga title..."/>
+                        </div>
+                    </div>
 
-                <div className="homescreen-decoration">
-                    <img src={Ichigo}></img>
-                </div>
+                    <div className="homescreen-decoration">
+                        <img src={Ichigo}></img>
+                    </div>
 
+                    
+
+                </div>
+                
                 <div className="homescreen-toplist">
                     <div className="homescreen-toplist-tarjeta">
                         <div className="homescreen-toplist-title">
                             <h3>Ranking</h3>
                         </div>
+                        <p>1. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
+                        <p>2. ADBSAJDBSA</p>
                     </div>
                 </div>
             </div>
